3d38f10c3ffda6dcf5a934483432fa7992701775
spaceshark123 <amogh.patnaik@icloud.com>
2023-05-06 12:55:39 -0500

    Initial commit

---
+++ src/main/java/net/spaceshark123/firstmod/FirstModClient.java
@@ -0,0 +1,11 @@
+package net.spaceshark123.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
