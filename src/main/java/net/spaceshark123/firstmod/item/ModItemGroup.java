ef7f6299518384ffa865bfc147ac5a2769830ea7
spaceshark123 <amogh.patnaik@icloud.com>
2023-05-07 07:52:17 -0500

    added custom item: raw tanzanite, and created a custom item group following a tutorial

--- src/main/java/net/spaceshark123/firstmod/item/ModItemGroup.java
+++ src/main/java/net/spaceshark123/firstmod/item/ModItemGroup.java
@@ -4,7 +4,6 @@ package net.spaceshark123.firstmod.item;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.spaceshark123.firstmod.FirstMod;
 
--- src/main/java/net/spaceshark123/firstmod/item/ModItems.java
+++ src/main/java/net/spaceshark123/firstmod/item/ModItems.java
@@ -1,10 +1,8 @@
 package net.spaceshark123.firstmod.item;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
